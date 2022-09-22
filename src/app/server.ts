bfca5d180e17ef2bcd8244e312c81ed2e7787112
Augusto Guilarducci <augustofgui@gmail.com>
2022-09-22 14:44:15 -0300

    Testing railway

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import router from '../http/routes';
 
 const app = express();
+const port = process.env.PORT || 4003;
 
 app.use(express.json());
 app.use(router);
 
-app.listen(4003, () => console.log('🚀 Server is runnning in PORT 4003'));
+app.listen(port, () => console.log('🚀 Server is runnning in PORT 4003'));
