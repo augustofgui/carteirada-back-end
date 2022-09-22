ce59ab33cd62ef898ab783d0e9395b024f7afe81
Augusto Guilarducci <augustofgui@gmail.com>
2022-09-22 14:04:19 -0300

    Starting building process

--- src/tests/user.spec.ts
+++ src/tests/user.spec.ts
@@ -3,13 +3,11 @@ import { User } from '../core/entities/user';
 
 test('create an user', () => {
   const user = new User({
-    id: '0',
     name: 'John Doe',
     login: 'johndoe123',
     email: 'john@doe.com',
     password: '12341234',
     telephone: '12341234',
-    createdAt: new Date(),
   });
 
   expect(user).toBeInstanceOf(User);
