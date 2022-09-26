d4d33f2e3f95800a8402ef6183409dbd773c2f9c
Augusto Guilarducci <augustofgui@gmail.com>
2022-09-26 15:57:35 -0300

    Added token provider container

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -3,9 +3,12 @@ import IUsersRepository from '@core/repositories/IUsersRepository';
 import PrismaUsersRepository from '@http/repositories/PrismaUsersRepository';
 import BCryptHashProvider from '@infra/providers/HashProvider/implementations/BCryptHashProvider';
 import IHashProvider from '@infra/providers/HashProvider/models/IHashProvider';
+import ITokenProvider from '@infra/providers/TokenProvider/models/ITokenProvider';
+import JWTTokenProvider from '@infra/providers/TokenProvider/implementations/JWTTokenProvider';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   PrismaUsersRepository
 );
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+container.registerSingleton<ITokenProvider>('TokenProvider', JWTTokenProvider);
